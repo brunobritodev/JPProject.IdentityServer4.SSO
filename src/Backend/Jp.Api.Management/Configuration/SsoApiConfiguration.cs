7da70a46a0138a84614581e7d0df218143a04c45
Bruno Brito <bhdebrito@gmail.com>
2019-11-03 00:45:33 -0300

    beta testing

--- src/Backend/Jp.Api.Management/Configuration/SsoApiConfiguration.cs
+++ src/Backend/Jp.Api.Management/Configuration/SsoApiConfiguration.cs
@@ -1,13 +1,11 @@
 ﻿using AutoMapper;
 using AutoMapper.Configuration;
 using JPProject.Admin.Application.AutoMapper;
+using JPProject.Admin.Database;
 using JPProject.AspNet.Core;
-using JPProject.EntityFrameworkCore.Configuration;
+using JPProject.Domain.Core.ViewModels;
 using JPProject.Sso.Application.AutoMapper;
-using JPProject.Sso.EntityFrameworkCore.MySql.Configuration;
-using JPProject.Sso.EntityFrameworkCore.PostgreSQL.Configuration;
-using JPProject.Sso.EntityFrameworkCore.Sqlite.Configuration;
-using JPProject.Sso.EntityFrameworkCore.SqlServer.Configuration;
+using JPProject.Sso.Database;
 using MediatR;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -20,31 +18,14 @@ namespace Jp.Api.Management.Configuration
     {
         public static IServiceCollection ConfigureSsoApi(this IServiceCollection services, IConfiguration configuration)
         {
-            var database = configuration["ApplicationSettings:DatabaseType"].ToUpper();
+            var database = configuration.GetValue<DatabaseType>("ApplicationSettings:DatabaseType");
             var connString = configuration.GetConnectionString("SSOConnection");
-            var userIdentityBuilder = services.ConfigureUserIdentity<AspNetUser>();
-            var identityServerApiBuilder = services.ConfigureJpAdmin<AspNetUser>();
 
-            switch (database)
-            {
-                case "MYSQL":
-                    userIdentityBuilder.WithMySql<Startup>(connString).AddEventStoreMySql<Startup>(connString, EventStoreMigrationOptions.Get().ShouldMigrate(false));
-                    identityServerApiBuilder.WithMySql<Startup>(connString);
-                    break;
-                case "SQLSERVER":
-                    userIdentityBuilder.WithSqlServer<Startup>(connString).AddEventStoreSqlServer<Startup>(connString, EventStoreMigrationOptions.Get().ShouldMigrate(false));
-                    identityServerApiBuilder.WithSqlServer<Startup>(connString);
+            services.ConfigureUserIdentity<AspNetUser>().AddDatabase(database, connString);
 
-                    break;
-                case "POSTGRESQL":
-                    userIdentityBuilder.WithPostgreSql<Startup>(connString).AddEventStorePostgreSql<Startup>(connString, EventStoreMigrationOptions.Get().ShouldMigrate(false));
-                    identityServerApiBuilder.WithPostgreSql<Startup>(connString);
-                    break;
-                case "SQLITE":
-                    userIdentityBuilder.WithSqlite<Startup>(connString).AddEventStoreSqlite<Startup>(connString, EventStoreMigrationOptions.Get().ShouldMigrate(false));
-                    identityServerApiBuilder.WithSqlite<Startup>(connString);
-                    break;
-            }
+            services.ConfigureJpAdmin<AspNetUser>().AddDatabase(database, connString);
+
+
             return services;
         }
 
--- src/Frontend/Jp.UI.SSO/Configuration/SsoConfiguration.cs
+++ src/Frontend/Jp.UI.SSO/Configuration/SsoConfiguration.cs
@@ -1,12 +1,8 @@
 ﻿using AutoMapper;
 using AutoMapper.Configuration;
 using JPProject.AspNet.Core;
-using JPProject.EntityFrameworkCore.Configuration;
 using JPProject.Sso.Application.AutoMapper;
-using JPProject.Sso.EntityFrameworkCore.MySql.Configuration;
-using JPProject.Sso.EntityFrameworkCore.PostgreSQL.Configuration;
-using JPProject.Sso.EntityFrameworkCore.Sqlite.Configuration;
-using JPProject.Sso.EntityFrameworkCore.SqlServer.Configuration;
+using JPProject.Sso.Database;
 using MediatR;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -19,48 +15,15 @@ namespace Jp.UI.SSO.Configuration
     {
         public static void ConfigureSso(this IServiceCollection services, IConfiguration configuration)
         {
-            var database = configuration["ApplicationSettings:DatabaseType"].ToUpper();
+            var database = configuration.GetValue<DatabaseType>("ApplicationSettings:DatabaseType");
             var connString = configuration.GetConnectionString("SSOConnection");
-            var eventstoreOptions = EventStoreMigrationOptions.Get().ShouldMigrate(false);
-            switch (database)
-            {
-                case "MYSQL":
-                    services
-                        .ConfigureUserIdentity<AspNetUser>()
-                        .WithMySql<Startup>(connString)
-                        .AddEventStoreMySql<Startup>(connString, eventstoreOptions)
-                        .ConfigureIdentityServer()
-                        .WithMySql<Startup>(connString)
-                        .AddSigninCredentialFromConfig(configuration.GetSection("CertificateOptions"));
-                    break;
-                case "SQLSERVER":
-                    services
-                        .ConfigureUserIdentity<AspNetUser>()
-                        .WithSqlServer<Startup>(connString)
-                        .AddEventStoreSqlServer<Startup>(connString, eventstoreOptions)
-                        .ConfigureIdentityServer()
-                        .WithSqlServer<Startup>(connString)
-                        .AddSigninCredentialFromConfig(configuration.GetSection("CertificateOptions"));
-                    break;
-                case "POSTGRESQL":
-                    services
-                        .ConfigureUserIdentity<AspNetUser>()
-                        .WithPostgreSql<Startup>(connString)
-                        .AddEventStorePostgreSql<Startup>(connString, eventstoreOptions)
-                        .ConfigureIdentityServer()
-                        .WithPostgreSql<Startup>(connString)
-                        .AddSigninCredentialFromConfig(configuration.GetSection("CertificateOptions"));
-                    break;
-                case "SQLITE":
-                    services
-                        .ConfigureUserIdentity<AspNetUser>()
-                        .WithSqlite<Startup>(connString)
-                        .AddEventStoreSqlite<Startup>(connString, eventstoreOptions)
-                        .ConfigureIdentityServer()
-                        .WithSqlite<Startup>(connString)
-                        .AddSigninCredentialFromConfig(configuration.GetSection("CertificateOptions"));
-                    break;
-            }
+
+            services
+                .ConfigureUserIdentity<AspNetUser>().AddDatabase(database, connString)
+
+                .ConfigureIdentityServer()
+                .AddSigninCredentialFromConfig(configuration.GetSection("CertificateOptions"))
+                .AddOAuth2Database(database, connString);
 
             var configurationExpression = new MapperConfigurationExpression();
             SsoMapperConfig.RegisterMappings().ForEach(p => configurationExpression.AddProfile(p));
