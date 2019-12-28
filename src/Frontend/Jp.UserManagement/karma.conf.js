31f083142cf4348cbc4d3e5cf330655ac9d17bd9
Athanasios Eleftheriadis <athanasios.eleftheriadis@gmail.com>
2019-12-28 14:59:02 +0200

    incomplete commit -f greek translations

--- src/Frontend/Jp.UserManagement/karma.conf.js
+++ src/Frontend/Jp.UserManagement/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, 'coverage'), reports: [ 'html', 'lcovonly' ],
+      dir: require('path').join(__dirname, 'coverage'), reports: [ 'html', 'lcovonly', 'text-summary' ],
       fixWebpackSourcePaths: true
     },
     angularCli: {
@@ -28,6 +28,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false
+    singleRun: false,
+    restartOnFileChange: true
   });
 };
